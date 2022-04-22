0d8e32ffcf088e945593386fac778a15119a91ad
Sharif Apu <sharifapu@Sharifs-MacBook-Pro.local>
2022-04-22 10:19:50 +0600

    Initial Commit

---
+++ modelDefinitions/NoiseDPN_GEN.py
@@ -0,0 +1,87 @@
+import torch.nn.functional as F
+import torch.nn as nn
+from torchsummary import summary
+from modelDefinitions.basicBlocksDPN import *
+
+class Noise_DPN(nn.Module):
+    def __init__(self):
+        super(Noise_DPN, self).__init__()
+
+        self.inConv = nn.Conv2d(3,64, 3,1,1)
+        self.FM0 = FMModule(64)
+
+        self.FM1 = FMModule(64)
+        self.gate1 = GatedConv2d(64, 64, 3, padding=1)
+        self.down1 = nn.Conv2d(64,96, kernel_size=3, stride=2, padding=1)
+
+        self.FM2 = FMModule(96)
+        self.gate2 = GatedConv2d(96, 96, 3, padding=1)
+        self.down2 = nn.Conv2d(96,128, kernel_size=3, stride=2, padding=1)
+        
+        self.FM3 = FMModule(128)
+        self.gate3 = GatedConv2d(128, 128, 3, padding=1)
+        self.down3 = nn.Conv2d(128, 160, kernel_size=3, stride=2, padding=1)
+
+        self.FM4 = FMModule(160)
+        self.gate4 = GatedConv2d(160, 160, 3, padding=1)
+        self.down4 = nn.Conv2d(160, 192, kernel_size=3, stride=2, padding=1)
+
+
+        self.FM5 = FMModule(192)
+        self.gate5 = GatedConv2d(192, 192, 3, padding=1)
+        self.up1 = nn.ConvTranspose2d(192, 160, 2, 2)
+
+        self.FM6 = FMModule(160)
+        self.up2 = nn.ConvTranspose2d(160, 128, 2,2)
+
+        self.FM7 = FMModule(128)
+        self.up3 = nn.ConvTranspose2d(128, 96, 2,2)
+
+        self.FM8 = FMModule(96)
+        self.up4 = nn.ConvTranspose2d(96, 64, 2,2)
+        
+        self.FM9 = FMModule(64)
+
+        self.outc = nn.Conv2d(64,3,1,)
+
+    def forward(self, x):
+
+        x1_1 = self.inConv(x)
+        #x1_2 = self.FM0(x1_1)  # 
+
+        x2_1 = self.FM1(x1_1)
+        g_2 = self.gate1(x2_1)
+        x2_2 = F.relu(self.down1(x2_1))
+
+        x3_1 = self.FM2(x2_2)
+        g_3 = self.gate2(x3_1)
+        x3_2 = F.relu(self.down2(x3_1))
+
+        x4_1 = self.FM3(x3_2)
+        g_4 = self.gate3(x4_1)
+        x4_2 = F.relu(self.down3(x4_1))
+
+        x5_1 = self.FM4(x4_2)
+        g_5 = self.gate4(x5_1)
+        x5_2 = F.relu(self.down4(x5_1))
+
+        x6_1 = self.FM5(x5_2)
+        x6_2 = F.relu(self.up1(x6_1)) + g_5
+
+        x7_1 = self.FM6(x6_2)
+        x7_2 = F.relu(self.up2(x7_1)) + g_4
+
+        x8_1 = self.FM7(x7_2)
+        x8_2 = F.relu(self.up3(x8_1)) + g_3
+
+        x9_1 = self.FM8(x8_2)
+        x9_2 = F.relu(self.up4(x9_1)) + g_2
+
+        #x10 = self.FM9(x9_2) #
+        out = torch.tanh(self.outc(x9_2) + x)
+
+        return out#self.outc(x9_2) + x
+
+
+#net = Noise_DPN()
+#summary(net, input_size = (3, 256, 256))
---
+++ modelDefinitions/basicBlocksDPN.py
@@ -0,0 +1,128 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torch.nn.init as init
+from torchsummary import summary
+
+'''class FMModule(nn.Module):
+    def __init__(self,in_channels,kernel_size=3,stride=1,padding=1, inputLayer=False):
+        super(FMModule,self).__init__()
+        if inputLayer == True:
+            #print("input true")
+            out_channel = in_channels
+            self.conv1 = nn.Conv2d(3,in_channels,kernel_size,stride,padding)
+            
+        else:
+            out_channel = int (in_channels * 1.5)
+            self.conv1 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding)
+        self.conv1_relu = nn.PReLU()
+
+        self.conv2 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding)
+        self.conv2_relu = nn.PReLU()
+
+        self.conv3 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding)
+        self.conv3_relu = nn.PReLU()
+
+    def forward(self,x):
+        x1 = self.conv1_relu(self.conv1(x))
+        x2 = self.conv2_relu(self.conv2(x1))
+        x3 = self.conv3(x2) + x1
+        xOut =  self.conv3_relu(x3)
+        return xOut'''
+
+
+class FMModule(nn.Module):
+    def __init__(self, in_features):
+        super(FMModule, self).__init__()
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=3, stride=1, padding=1),
+            #nn.BatchNorm2d(in_features, 0.8),
+            nn.PReLU(),
+            nn.Conv2d(in_features, in_features, kernel_size=3, stride=1, padding=1),
+            #nn.BatchNorm2d(in_features, 0.8),
+        )
+
+    def forward(self, x):
+        return x + self.conv_block(x)
+
+class GatedConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride = 1, padding = 0, dilation = 1, pad_type = 'reflect', activation = 'lrelu', norm = 'none', sn = False):
+        super(GatedConv2d, self).__init__()
+        # Initialize the padding scheme
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == 'replicate':
+            self.pad = nn.ReplicationPad2d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+        
+        # Initialize the normalization type
+        if norm == 'bn':
+            self.norm = nn.BatchNorm2d(out_channels)
+        elif norm == 'in':
+            self.norm = nn.InstanceNorm2d(out_channels)
+        elif norm == 'ln':
+            self.norm = LayerNorm(out_channels)
+        elif norm == 'none':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+        
+        # Initialize the activation funtion
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace = True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace = True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace = True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'sigmoid':
+            self.activation = nn.Sigmoid()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # Initialize the convolution layers
+        if sn:
+            self.conv2d = SpectralNorm(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation))
+            self.mask_conv2d = SpectralNorm(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation))
+        else:
+            self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation)
+            self.mask_conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation)
+        self.sigmoid = torch.nn.Sigmoid()
+    
+    def forward(self, x):
+        x = self.pad(x)
+        conv = self.conv2d(x)
+        mask = self.mask_conv2d(x)
+        gated_mask = self.sigmoid(mask)
+        x = conv * gated_mask
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
